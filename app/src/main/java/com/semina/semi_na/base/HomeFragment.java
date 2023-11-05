c8eaa62a6da9578621e19ac7f62f273a3f93a5b0
2wndrhs <2wndrhs@gmail.com>
2023-11-06 00:00:07 +0900

    feat: 전공별 세미나 탐색뷰 구현

--- app/src/main/java/com/semina/semi_na/base/HomeFragment.java
+++ app/src/main/java/com/semina/semi_na/base/HomeFragment.java
@@ -2,13 +2,26 @@ package com.semina.semi_na.base;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.viewpager2.widget.ViewPager2;
+
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
 import com.semina.semi_na.R;
+import com.semina.semi_na.view.adapter.CollegePagerAdapter;
+
+import org.w3c.dom.Text;
+
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -25,6 +38,19 @@ public class HomeFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+    private LinkedHashMap tabTitles = new LinkedHashMap() {{
+        put("IT대학", R.drawable.it_college_icon);
+        put("공과대학", R.drawable.engineering_college_icon);
+        put("자연과학대학", R.drawable.natural_science_college_icon);
+        put("사회과학대학", R.drawable.social_science_college_icon);
+        put("인문사회대학", R.drawable.humanity_college_icon);
+        put("법과대학", R.drawable.law_college_icon);
+        put("경제통상대학", R.drawable.economics_college_icon);
+        put("경영대학", R.drawable.business_college_icon);
+    }};
+    private TabLayout tabLayout;
+    private ViewPager2 viewPager;
+    private CollegePagerAdapter pagerAdapter;
 
     public HomeFragment() {
         // Required empty public constructor
@@ -58,9 +84,25 @@ public class HomeFragment extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        // get keys of tabTitles
+        String[] titles = (String[]) tabTitles.keySet().toArray(new String[tabTitles.size()]);
+
+        tabLayout = view.findViewById(R.id.tabLayout);
+        viewPager = view.findViewById(R.id.viewPager);
+        pagerAdapter = new CollegePagerAdapter(this);
+        viewPager.setAdapter(pagerAdapter);
+
+        new TabLayoutMediator(tabLayout, viewPager, (tab, position) -> {
+            tab.setText(titles[position]);
+            tab.setIcon((Integer) tabTitles.get(titles[position]));
+        }).attach();
     }
 }
---
+++ app/src/main/java/com/semina/semi_na/view/adapter/CollegePagerAdapter.java
@@ -0,0 +1,48 @@
+package com.semina.semi_na.view.adapter;
+
+import androidx.fragment.app.Fragment;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+import com.semina.semi_na.view.home.BusinessFragment;
+import com.semina.semi_na.view.home.EconomicsFragment;
+import com.semina.semi_na.view.home.EngineeringFragment;
+import com.semina.semi_na.view.home.HumanityFragment;
+import com.semina.semi_na.view.home.ITFragment;
+import com.semina.semi_na.view.home.LawFragment;
+import com.semina.semi_na.view.home.NaturalScienceFragment;
+import com.semina.semi_na.view.home.SocialScienceFragment;
+
+public class CollegePagerAdapter extends FragmentStateAdapter {
+    public CollegePagerAdapter(Fragment fa) {
+        super(fa);
+    }
+
+    @Override
+    public int getItemCount() {
+        return 8;
+    }
+
+    @Override
+    public androidx.fragment.app.Fragment createFragment(int position) {
+        switch (position) {
+            case 0:
+                return new ITFragment();
+            case 1:
+                return new EngineeringFragment();
+            case 2:
+                return new NaturalScienceFragment();
+            case 3:
+                return new SocialScienceFragment();
+            case 4:
+                return new HumanityFragment();
+            case 5:
+                return new LawFragment();
+            case 6:
+                return new EconomicsFragment();
+            case 7:
+                return new BusinessFragment();
+            default:
+                return new ITFragment();
+        }
+    }
+}
