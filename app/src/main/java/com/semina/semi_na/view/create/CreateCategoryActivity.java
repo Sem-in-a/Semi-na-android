9dc6f4a75bf3f54bf7473efaef052873401669c8
JjungminLee <julie0964@naver.com>
2023-11-05 11:35:16 +0900

    feat:OpenAI연결 짜잘짜잘

--- app/src/main/java/com/semina/semi_na/view/create/CreateCategoryActivity.java
+++ app/src/main/java/com/semina/semi_na/view/create/CreateCategoryActivity.java
@@ -5,12 +5,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.semina.semi_na.R;
+import com.semina.semi_na.databinding.ActivityCreateCategoryBinding;
+import com.semina.semi_na.databinding.ActivityCreateTitleBinding;
 
 public class CreateCategoryActivity extends AppCompatActivity {
 
+
+    ActivityCreateCategoryBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_create_category);
+        binding = ActivityCreateCategoryBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
     }
 }
