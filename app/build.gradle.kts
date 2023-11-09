41d4320982cc02cce7639aad43171969ed70b5db
choiseohyeon <csh200310@naver.com>
2023-11-10 02:24:42 +0900

    mypage

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,6 +3,7 @@ import com.android.build.gradle.internal.cxx.configure.gradleLocalProperties
 plugins {
     id("com.android.application")
     id("com.google.gms.google-services")
+    id("org.jetbrains.kotlin.android")
 }
 
 android {
@@ -21,6 +22,9 @@ android {
 
         // local.properties 내부에서 key값을 가져오는 함수 구현방식
         buildConfigField("String", "OPENAI_KEY", getApiKey("OPENAI_KEY"))
+        vectorDrawables {
+            useSupportLibrary = true
+        }
     }
 
     buildTypes {
@@ -43,6 +47,19 @@ android {
     }
     buildFeatures {
         buildConfig = true
+        viewBinding = true
+        compose = true
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.4.3"
+    }
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
     }
 
 }
@@ -71,9 +88,26 @@ dependencies {
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("com.google.firebase:firebase-database:20.3.0")
     implementation("com.google.firebase:firebase-firestore:24.9.1")
+    implementation("androidx.legacy:legacy-support-v4:1.0.0")
+    implementation("androidx.recyclerview:recyclerview:1.3.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
+    implementation("androidx.activity:activity-compose:1.8.0")
+    implementation(platform("androidx.compose:compose-bom:2023.03.00"))
+    implementation("androidx.compose.ui:ui")
+    implementation("androidx.compose.ui:ui-graphics")
+    implementation("androidx.compose.ui:ui-tooling-preview")
+    implementation("androidx.compose.material3:material3")
+    implementation(platform("androidx.compose:compose-bom:2023.03.00"))
+    implementation(platform("androidx.compose:compose-bom:2023.03.00"))
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
+    androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
+    androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
+    debugImplementation("androidx.compose.ui:ui-tooling")
+    debugImplementation("androidx.compose.ui:ui-test-manifest")
 }
 
 // 2. local.properties 내부에서 key값을 가져오는 함수 구현방식
--- app/src/main/java/com/semina/semi_na/base/MyPageFragment.java
+++ app/src/main/java/com/semina/semi_na/base/MyPageFragment.java
@@ -1,7 +1,14 @@
 package com.semina.semi_na.base;
 
+import static android.content.Context.MODE_PRIVATE;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
+import android.widget.ImageView;
+import android.widget.ScrollView;
+import android.widget.TextView;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -9,58 +16,55 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.semina.semi_na.R;
+import com.semina.semi_na.view.mypage.ViewDetailAppliedActivity;
+import com.semina.semi_na.view.mypage.ViewDetailHostedActivity;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link MyPageFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class MyPageFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+  private ImageView viewDetailHostedBtn;
+  private ImageView viewDetailAppliedBtn;
+  private ScrollView scrollView;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+  TextView studentNumTextView;
+  TextView departmentTextView;
+  TextView majorTextView;
+  TextView nameTextView;
 
-    public MyPageFragment() {
-        // Required empty public constructor
-    }
+  @Override
+  public View onCreateView(LayoutInflater inflater, ViewGroup container,
+      Bundle savedInstanceState) {
+    View view = inflater.inflate(R.layout.fragment_my_page, container, false);
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment MyPageFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static MyPageFragment newInstance(String param1, String param2) {
-        MyPageFragment fragment = new MyPageFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    scrollView = view.findViewById(R.id.scrollView);
+    viewDetailHostedBtn = view.findViewById(R.id.viewDetail_hosted);
+    viewDetailAppliedBtn = view.findViewById(R.id.viewDetail_applied);
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    studentNumTextView = view.findViewById(R.id.some_id_name);
+    departmentTextView = view.findViewById(R.id.some_id_under_department);
+    majorTextView = view.findViewById(R.id.some_id_major);
+    nameTextView = view.findViewById(R.id.some_id_grade);
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_my_page, container, false);
-    }
+    SharedPreferences preferences = getActivity().getSharedPreferences("UserInfo", MODE_PRIVATE);
+    String studentNum = preferences.getString("studentNum", "");
+    String department = preferences.getString("depart", "");
+    String major = preferences.getString("major", "");
+    String name = preferences.getString("name", "");
+
+    studentNumTextView.setText(studentNum);
+    departmentTextView.setText(department);
+    majorTextView.setText(major);
+    nameTextView.setText(name);
+
+    viewDetailHostedBtn.setOnClickListener(hostedView -> {
+      Intent intent = new Intent(getActivity(), ViewDetailHostedActivity.class);
+      startActivity(intent);
+    });
+
+    viewDetailAppliedBtn.setOnClickListener(appliedView -> {
+      Intent intent = new Intent(getActivity(), ViewDetailAppliedActivity.class);
+      startActivity(intent);
+    });
+
+    return view;
+  }
 }
