97365d675fe787142d189a2339dc2d3a1078bede
최서현 <104755384+candosh@users.noreply.github.com>
2023-11-10 12:17:02 +0900

    feat: 마이페이지 퍼블리싱 (#15)
    * feat/#4/mypage-profile
    
    * fix/xml-text
    
    * fix/마이페이지수정/의존성제거

--- app/src/main/java/com/semina/semi_na/base/MyPageFragment.java
+++ app/src/main/java/com/semina/semi_na/base/MyPageFragment.java
@@ -1,66 +1,52 @@
 package com.semina.semi_na.base;
 
+import static android.content.Context.MODE_PRIVATE;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-
 import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.semina.semi_na.R;
+import com.semina.semi_na.databinding.FragmentMyPageBinding;
+import com.semina.semi_na.view.mypage.ViewDetailAppliedActivity;
+import com.semina.semi_na.view.mypage.ViewDetailHostedActivity;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link MyPageFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class MyPageFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+  private FragmentMyPageBinding binding;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+  @Override
+  public View onCreateView(LayoutInflater inflater, ViewGroup container,
+      Bundle savedInstanceState) {
+    binding = FragmentMyPageBinding.inflate(inflater, container, false);
+    View view = binding.getRoot();
 
-    public MyPageFragment() {
-        // Required empty public constructor
-    }
+    SharedPreferences preferences = getActivity().getSharedPreferences("UserInfo", MODE_PRIVATE);
+    binding.someIdName.setText(preferences.getString("name", ""));
+    binding.someIdUnderDepartment.setText(preferences.getString("depart", ""));
+    binding.someIdMajor.setText(preferences.getString("major", ""));
+    binding.someIdGrade.setText(preferences.getString("studentNum", ""));
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment MyPageFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static MyPageFragment newInstance(String param1, String param2) {
-        MyPageFragment fragment = new MyPageFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    binding.viewDetailHosted.setOnClickListener(hostedView -> {
+      Intent intent = new Intent(getActivity(), ViewDetailHostedActivity.class);
+      startActivity(intent);
+    });
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    binding.viewDetailApplied.setOnClickListener(appliedView -> {
+      Intent intent = new Intent(getActivity(), ViewDetailAppliedActivity.class);
+      startActivity(intent);
+    });
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_my_page, container, false);
-    }
+    return view;
+  }
+
+  @Override
+  public void onDestroyView() {
+    super.onDestroyView();
+    binding = null;
+  }
 }
+
