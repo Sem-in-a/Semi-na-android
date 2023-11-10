0e0936a492f991dd1643b8a644ae67b4ea6d8800
choiseohyeon <csh200310@naver.com>
2023-11-10 12:11:27 +0900

    fix/마이페이지수정/의존성거

--- app/src/main/java/com/semina/semi_na/base/MyPageFragment.java
+++ app/src/main/java/com/semina/semi_na/base/MyPageFragment.java
@@ -5,66 +5,48 @@ import static android.content.Context.MODE_PRIVATE;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-
-import android.widget.ImageView;
-import android.widget.ScrollView;
-import android.widget.TextView;
 import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.semina.semi_na.R;
+import com.semina.semi_na.databinding.FragmentMyPageBinding;
 import com.semina.semi_na.view.mypage.ViewDetailAppliedActivity;
 import com.semina.semi_na.view.mypage.ViewDetailHostedActivity;
 
 public class MyPageFragment extends Fragment {
 
-  private ImageView viewDetailHostedBtn;
-  private ImageView viewDetailAppliedBtn;
-  private ScrollView scrollView;
-
-  TextView studentNumTextView;
-  TextView departmentTextView;
-  TextView majorTextView;
-  TextView nameTextView;
+  private FragmentMyPageBinding binding;
 
   @Override
   public View onCreateView(LayoutInflater inflater, ViewGroup container,
       Bundle savedInstanceState) {
-    View view = inflater.inflate(R.layout.fragment_my_page, container, false);
-
-    scrollView = view.findViewById(R.id.scrollView);
-    viewDetailHostedBtn = view.findViewById(R.id.viewDetail_hosted);
-    viewDetailAppliedBtn = view.findViewById(R.id.viewDetail_applied);
-
-    studentNumTextView = view.findViewById(R.id.some_id_name);
-    departmentTextView = view.findViewById(R.id.some_id_under_department);
-    majorTextView = view.findViewById(R.id.some_id_major);
-    nameTextView = view.findViewById(R.id.some_id_grade);
+    binding = FragmentMyPageBinding.inflate(inflater, container, false);
+    View view = binding.getRoot();
 
     SharedPreferences preferences = getActivity().getSharedPreferences("UserInfo", MODE_PRIVATE);
-    String studentNum = preferences.getString("studentNum", "");
-    String department = preferences.getString("depart", "");
-    String major = preferences.getString("major", "");
-    String name = preferences.getString("name", "");
+    binding.someIdName.setText(preferences.getString("name", ""));
+    binding.someIdUnderDepartment.setText(preferences.getString("depart", ""));
+    binding.someIdMajor.setText(preferences.getString("major", ""));
+    binding.someIdGrade.setText(preferences.getString("studentNum", ""));
 
-    studentNumTextView.setText(studentNum);
-    departmentTextView.setText(department);
-    majorTextView.setText(major);
-    nameTextView.setText(name);
-
-    viewDetailHostedBtn.setOnClickListener(hostedView -> {
+    binding.viewDetailHosted.setOnClickListener(hostedView -> {
       Intent intent = new Intent(getActivity(), ViewDetailHostedActivity.class);
       startActivity(intent);
     });
 
-    viewDetailAppliedBtn.setOnClickListener(appliedView -> {
+    binding.viewDetailApplied.setOnClickListener(appliedView -> {
       Intent intent = new Intent(getActivity(), ViewDetailAppliedActivity.class);
       startActivity(intent);
     });
 
     return view;
   }
+
+  @Override
+  public void onDestroyView() {
+    super.onDestroyView();
+    binding = null;
+  }
 }
+
