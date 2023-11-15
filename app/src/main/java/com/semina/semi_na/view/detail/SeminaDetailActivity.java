a101d366af6b5af7a7e0692ce607bcbedc2e6c11
2wndrhs <2wndrhs@gmail.com>
2023-11-15 16:54:31 +0900

    feat: 상단 카드 뷰 구현

---
+++ app/src/main/java/com/semina/semi_na/view/detail/SeminaDetailActivity.java
@@ -0,0 +1,14 @@
+package com.semina.semi_na.view.detail;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.semina.semi_na.R;
+
+public class SeminaDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_semina_detail);
+    }
+}
