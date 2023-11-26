b6d7964585f9eb1e5210211ea8d39d9264f8f846
JungminLee <85864699+JjungminLee@users.noreply.github.com>
2023-11-26 20:20:48 +0900

    fix:fcm 로직 수정 이것저것 (#27)
    * fix:fcm 로직 수정 이것저것
    
    * docs:README 수정

--- app/src/main/java/com/semina/semi_na/view/create/CreateCompleteActivity.java
+++ app/src/main/java/com/semina/semi_na/view/create/CreateCompleteActivity.java
@@ -2,15 +2,33 @@ package com.semina.semi_na.view.create;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.semina.semi_na.R;
+import com.semina.semi_na.base.MainActivity;
+import com.semina.semi_na.databinding.ActivityCreateCategoryBinding;
+import com.semina.semi_na.databinding.ActivityCreateCompleteBinding;
+import com.semina.semi_na.databinding.ActivityCreateMemberCountBinding;
 
 public class CreateCompleteActivity extends AppCompatActivity {
+
+    ActivityCreateCompleteBinding binding;
+    private Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_create_complete);
+        binding = ActivityCreateCompleteBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        binding.activityCreateCompleteNextBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
