20b153809ef0039b985ce24004797f55ffc0b494
choiseohyeon <csh200310@naver.com>
2023-11-26 11:56:58 +0900

    feat/초기api연결

--- app/src/main/java/com/semina/semi_na/base/MyPageFragment.java
+++ app/src/main/java/com/semina/semi_na/base/MyPageFragment.java
@@ -11,26 +11,34 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.semina.semi_na.databinding.FragmentMyPageBinding;
 import com.semina.semi_na.view.mypage.LogoutModalFragment;
 import com.semina.semi_na.view.mypage.ViewDetailAppliedActivity;
 import com.semina.semi_na.view.mypage.ViewDetailHostedActivity;
 
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+
 public class MyPageFragment extends Fragment {
 
   private FragmentMyPageBinding binding;
+  FirebaseFirestore db = FirebaseFirestore.getInstance();
 
   @Override
   public View onCreateView(LayoutInflater inflater, ViewGroup container,
       Bundle savedInstanceState) {
     binding = FragmentMyPageBinding.inflate(inflater, container, false);
     View view = binding.getRoot();
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
 
     SharedPreferences preferences = getActivity().getSharedPreferences("UserInfo", MODE_PRIVATE);
     binding.someIdName.setText(preferences.getString("name", ""));
     binding.someIdUnderDepartment.setText(preferences.getString("depart", ""));
     binding.someIdMajor.setText(preferences.getString("major", ""));
     binding.someIdGrade.setText(preferences.getString("studentNum", ""));
+    String studentNum = preferences.getString("studentNum", "");
+
 
     binding.viewDetailHosted.setOnClickListener(hostedView -> {
       Intent intent = new Intent(getActivity(), ViewDetailHostedActivity.class);
@@ -47,8 +55,23 @@ public class MyPageFragment extends Fragment {
       logoutDialog.show(getParentFragmentManager(), "logoutDialog");
     });
 
+
+    // Firestore에서 해당 사용자의 신청 목록 조회
+    Query query = db.collection("Applications").whereEqualTo("studentNum", studentNum);
+    query.get().addOnCompleteListener(task -> {
+      if (task.isSuccessful()) {
+        for (DocumentSnapshot document : task.getResult()) {
+          // 여기서 각 문서를 사용하여 데이터 처리
+          // 예: SeminaApplication application = document.toObject(SeminaApplication.class);
+        }
+      } else {
+        // 쿼리 실패 처리
+      }
+    });
+
     return view;
   }
+
 
   @Override
   public void onDestroyView() {
