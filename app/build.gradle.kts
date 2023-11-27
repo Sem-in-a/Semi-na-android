b00d80df874b60e42184f1d551cfcef0be16a4b1
2wndrhs <2wndrhs@gmail.com>
2023-11-28 04:29:35 +0900

    build: 불필요한 의존성 제거

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,8 +28,8 @@ android {
         release {
             isMinifyEnabled = false
             proguardFiles(
-                    getDefaultProguardFile("proguard-android-optimize.txt"),
-                    "proguard-rules.pro"
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
 
             )
 
@@ -37,9 +37,9 @@ android {
     }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility =  JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
     }
-    viewBinding{
+    viewBinding {
         enable = true
     }
     buildFeatures {
@@ -51,24 +51,22 @@ android {
 dependencies {
 
     // Retrofit2
-    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
 
     // Import the Firebase BoM
-    implementation(platform("com.google.firebase:firebase-bom:32.3.1"))
-    implementation("com.google.firebase:firebase-analytics-ktx")
-    implementation("com.google.firebase:firebase-firestore-ktx:24.9.1")
+    implementation(platform("com.google.firebase:firebase-bom:32.5.0"))
+
+    // TODO: Add the dependencies for Firebase products you want to use
+    // When using the BoM, don't specify versions in Firebase dependencies
+    implementation("com.google.firebase:firebase-analytics")
+
     // Add the dependency for the Cloud Storage library
     // When using the BoM, you don't specify versions in Firebase library dependencies
     implementation("com.google.firebase:firebase-storage")
 
     //OpenAI
     implementation("com.theokanning.openai-gpt3-java:service:0.16.1")
-
-    //Recycler view
-    implementation("androidx.recyclerview:recyclerview:1.3.2")
-    // For control over item selection of both touch and mouse driven selection
-    implementation("androidx.recyclerview:recyclerview-selection:1.1.0")
 
     //Glide
     implementation("com.github.bumptech.glide:glide:4.16.0")
@@ -77,12 +75,16 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("com.google.firebase:firebase-database:20.3.0")
-    implementation("com.google.firebase:firebase-firestore:24.9.1")
-    implementation("com.google.firebase:firebase-messaging:23.3.1")
+    implementation("com.google.firebase:firebase-database")
+    implementation("com.google.firebase:firebase-firestore")
+    implementation("com.google.firebase:firebase-messaging")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    // FirebaseUI for Cloud Firestore
+    implementation("com.firebaseui:firebase-ui-firestore:8.0.2")
+    implementation("androidx.paging:paging-runtime:3.2.1")
 }
 
 // 2. local.properties 내부에서 key값을 가져오는 함수 구현방식
@@ -90,5 +92,6 @@ fun getApiKey(propertyKey: String): String {
     val gradleLocalProperties = gradleLocalProperties(rootDir)
     val propertyValue = gradleLocalProperties.getProperty(propertyKey)
 
-    return propertyValue ?: throw IllegalStateException("Property '$propertyKey' not found in gradleLocalProperties")
+    return propertyValue
+        ?: throw IllegalStateException("Property '$propertyKey' not found in gradleLocalProperties")
 }
