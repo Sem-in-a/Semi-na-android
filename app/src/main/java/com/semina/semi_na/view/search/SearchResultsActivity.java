5992c37b55b66878a8b3551a1b47a81e989dd889
choiseohyeon <csh200310@naver.com>
2023-11-29 23:35:16 +0900

    feat/검색창레이아웃

---
+++ app/src/main/java/com/semina/semi_na/view/search/SearchResultsActivity.java
@@ -0,0 +1,14 @@
+package com.semina.semi_na.view.search;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.semina.semi_na.R;
+
+public class SearchResultsActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_search_results);
+  }
+}
