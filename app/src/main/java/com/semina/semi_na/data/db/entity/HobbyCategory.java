7a277eec7b5beb0eee2c5bc3c701f05f1a3ab201
2wndrhs <2wndrhs@gmail.com>
2023-11-29 17:22:15 +0900

    feat: `MajorCategory`, `HobbyCategory` enum에 한글 이름 추가

--- app/src/main/java/com/semina/semi_na/data/db/entity/HobbyCategory.java
+++ app/src/main/java/com/semina/semi_na/data/db/entity/HobbyCategory.java
@@ -2,6 +2,15 @@ package com.semina.semi_na.data.db.entity;
 
 public enum HobbyCategory {
 
-    EXERCISE, FOOD, MUSIC, BOOK, NULL
+    EXERCISE("운동"), FOOD("요리"), MUSIC("음악"), BOOK("독서"), NULL("분류없음");
+    private String hobbyName;
+
+    HobbyCategory(String hobbyName) {
+        this.hobbyName = hobbyName;
+    }
+
+    public String getHobbyName() {
+        return hobbyName;
+    }
 
 }
