945e9f0faea043c42e3c23d687c1e2e31e88829b
choiseohyeon <csh200310@naver.com>
2023-12-04 09:52:19 +0900

    feat/마감된세미나

--- app/src/main/java/com/semina/semi_na/view/mypage/HostedClosedFragment.java
+++ app/src/main/java/com/semina/semi_na/view/mypage/HostedClosedFragment.java
@@ -5,29 +5,30 @@ import static android.content.Context.MODE_PRIVATE;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
-import androidx.annotation.NonNull;
-import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import androidx.paging.PagingConfig;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.GridLayoutManager;
-import com.firebase.ui.firestore.paging.FirestorePagingAdapter;
-import com.firebase.ui.firestore.paging.FirestorePagingOptions;
+import androidx.recyclerview.widget.RecyclerView;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.Query;
 import com.semina.semi_na.R;
 import com.semina.semi_na.data.db.entity.Semina;
 import com.semina.semi_na.databinding.FragmentHostedClosedBinding;
 import com.semina.semi_na.databinding.SeminarCardViewItemBinding;
 import com.semina.semi_na.view.viewHolder.SeminarCardViewHolder;
-import java.util.Date;
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.List;
 
-// 내가 주최한 세미나 - 마감 된 : 프론트 단에서 현재 날짜 비교
+
 public class HostedClosedFragment extends Fragment {
 
   private FragmentHostedClosedBinding binding;
-  private FirestorePagingAdapter<Semina, SeminarCardViewHolder> adapter;
+  private RecyclerView.Adapter<SeminarCardViewHolder> adapter;
+  private List<Semina> closedSeminarsList = new ArrayList<>();
 
   private String getCurrentUserId() {
     SharedPreferences preferences = getActivity().getSharedPreferences("UserInfo", MODE_PRIVATE);
@@ -35,72 +36,72 @@ public class HostedClosedFragment extends Fragment {
   }
 
   @Override
-  public View onCreateView(LayoutInflater inflater, ViewGroup container,
-      Bundle savedInstanceState) {
-
+  public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
     binding = FragmentHostedClosedBinding.inflate(inflater, container, false);
-    View view = binding.getRoot();
-
     String currentUserId = getCurrentUserId();
 
-    Date currentDate = new Date();
-    Query closedSeminaQuery = FirebaseFirestore.getInstance()
+    FirebaseFirestore.getInstance()
         .collection("Semina")
         .whereEqualTo("host", currentUserId)
-        .whereLessThanOrEqualTo("date", currentDate)
-        .orderBy("date", Query.Direction.DESCENDING);
+        .get()
+        .addOnSuccessListener(queryDocumentSnapshots -> {
+          for (DocumentSnapshot snapshot : queryDocumentSnapshots.getDocuments()) {
+            Semina semina = snapshot.toObject(Semina.class);
+            if (semina != null) {
+              try {
+                if (semina.isClosed()) {
+                  closedSeminarsList.add(semina);
+                }
+              } catch (ParseException e) {
+                Log.e("HostedClosedFragment", "Error parsing date", e);
+              }
+            }
+          }
+          setupRecyclerView();
+        })
+        .addOnFailureListener(e -> Log.e("HostedClosedFragment", "Error fetching data", e));
 
-    // 페이징 구성
-    PagingConfig config = new PagingConfig(10, 5, false);
+    return binding.getRoot();
+  }
 
-    // 어댑터 옵션 설정
-    FirestorePagingOptions<Semina> closedSeminaOptions = new FirestorePagingOptions.Builder<Semina>()
-        .setLifecycleOwner(this)
-        .setQuery(closedSeminaQuery, config, Semina.class)
-        .build();
-
-    // 어댑터 설정
-    adapter = new FirestorePagingAdapter<Semina, SeminarCardViewHolder>(closedSeminaOptions) {
-      @Override
-      protected void onBindViewHolder(@NonNull SeminarCardViewHolder holder, int position, @NonNull Semina model) {
-        holder.bind(model);
-        Log.d("HostedClosedFragment", "onBindViewHolder is called with position: " + position);
-      }
-
+  private void setupRecyclerView() {
+    adapter = new RecyclerView.Adapter<SeminarCardViewHolder>() {
       @NonNull
       @Override
       public SeminarCardViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        Log.d("HostedClosedFragment", "onCreateViewHolder is called");
         View itemView = LayoutInflater.from(parent.getContext())
             .inflate(R.layout.seminar_card_view_item, parent, false);
         SeminarCardViewItemBinding binding = SeminarCardViewItemBinding.bind(itemView);
         return new SeminarCardViewHolder(binding);
       }
+
+      @Override
+      public void onBindViewHolder(@NonNull SeminarCardViewHolder holder, int position) {
+        Semina semina = closedSeminarsList.get(position);
+        holder.bind(semina);
+      }
+
+      @Override
+      public int getItemCount() {
+        return closedSeminarsList.size();
+      }
     };
 
-    // 리사이클러뷰 설정
     binding.hosted2RecyclerView.setLayoutManager(new GridLayoutManager(getContext(), 2));
     binding.hosted2RecyclerView.setAdapter(adapter);
-
-    return view;
   }
 
   @Override
   public void onStart() {
     super.onStart();
-    if (adapter != null) {
-      adapter.startListening();
-    }
   }
 
   @Override
   public void onStop() {
     super.onStop();
-    if (adapter != null) {
-      adapter.stopListening();
-    }
   }
 }
+
 
 
 
